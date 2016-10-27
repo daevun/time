d81e76c02118cf73cd2d57d9429b8c2372f0de8e
Daniel Mihaylov <daniel.mihaylov85@gmail.com>
2016-10-27 22:06:37 +0200

    Gson Dependency und User Service cleanup
    Signed-off-by: Daniel Mihaylov <daniel.mihaylov85@gmail.com>

--- time/time_user/src/main/java/at/time/user/services/UserService.java
+++ time/time_user/src/main/java/at/time/user/services/UserService.java
@@ -1,18 +1,17 @@
 package at.time.user.services;
 
+import com.google.gson.Gson;
+
+import at.time.base.rabbit.RabbitManager;
 import at.time.user.dao.UserDao;
 import at.time.user.model.User;
 
 public class UserService {
 
-	private final UserDao dao;
+	private UserDao dao;
 
 	public UserService() {
-		dao = new UserDao();
-	}
-
-	public UserService(UserDao dao) {
-		this.dao = dao;
+		setDao(new UserDao());
 	}
 
 	public User createUser() {
@@ -21,10 +20,15 @@ public class UserService {
 
 	public void saveUser(final User user) {
 		dao.saveUser(user);
+		RabbitManager.getInstance().publishMessage(new Gson().toJson(user));
 	}
 
 	public User getUserByOid(final String oid) {
 		return dao.getByOid(oid);
+	}
+
+	public void setDao(UserDao dao) {
+		this.dao = dao;
 	}
 
 }
