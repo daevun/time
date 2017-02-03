1f2860714132942354f345133a855e0661806954
Daniel Mihaylov <daniel.mihaylov85@gmail.com>
2017-02-03 01:41:42 +0100

    Never meant to checkin this test
    Signed-off-by: Daniel Mihaylov <daniel.mihaylov85@gmail.com>

--- time/time_user/src/test/java/at/time/user/rest/RestServiceTest.java
+++ time/time_user/src/test/java/at/time/user/rest/RestServiceTest.java
@@ -1,16 +1,13 @@
 package at.time.user.rest;
 
-import static org.junit.Assert.assertNotNull;
-
 import org.junit.Test;
-
-import at.time.user.dao.UserDao;
 
 public class RestServiceTest {
 
-    @Test
-    public void testGetIt() {
-    	assertNotNull(new UserDao().getBySozVers("99"));
-    }
+	@Test
+	public void testGetIt() {
+		// never meant to be checked in this way - maybe do some proper testing
+		// assertNotNull(new UserDao().getBySozVers("99"));
+	}
 
 }
